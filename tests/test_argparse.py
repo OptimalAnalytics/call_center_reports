4ebb45671cc7d80c504b9ab0961575abd62977a6
eskemojoe007 <David.Folkner@gmail.com>
2018-06-22 16:55:47 -0400

    Beautify

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -1,13 +1,15 @@
 import pytest
 from process_reports import RPCArgParse
 import os
-from argparse import ArgumentTypeError, ArgumentError
+from argparse import ArgumentTypeError
 import datetime
+
 
 @pytest.fixture
 def rpcargparse():
     '''Returns a typical RPCArgParse'''
     return RPCArgParse()
+
 
 @pytest.fixture
 def descrip_RPCArgParse():
@@ -17,17 +19,20 @@ def descrip_RPCArgParse():
 
 def test_RPCArgParse_descrip1(rpcargparse):
     assert rpcargparse.description == 'Read in the main files to create RPC summary'
+
+
 def test_RPCArgParse_descrip2(descrip_RPCArgParse):
     assert descrip_RPCArgParse.description == 'Alternate descrip'
 
 
 bucket_fn_data = [
-    (os.path.join('Sample_Reports','2_1_18Bucket_Scrubbed.xls'),True),
-    (os.path.join('Sample_Reports','2_1_18Bucket_BAD.xls'),False),
+    (os.path.join('Sample_Reports', '2_1_18Bucket_Scrubbed.xls'), True),
+    (os.path.join('Sample_Reports', '2_1_18Bucket_BAD.xls'), False),
 ]
 
-@pytest.mark.parametrize("fn,is_file",bucket_fn_data)
-def test_is_valid_file(fn,is_file,rpcargparse):
+
+@pytest.mark.parametrize("fn,is_file", bucket_fn_data)
+def test_is_valid_file(fn, is_file, rpcargparse):
     '''
     Test the is_valid_file with the paraemtrized inputs.
     Should raise an error for a bad file and nothing for a good file.
@@ -36,16 +41,18 @@ def test_is_valid_file(fn,is_file,rpcargparse):
         with pytest.raises(ArgumentTypeError):
             rpcargparse.is_valid_file(fn)
 
+
 @pytest.mark.xpass(strict=True)
 def test_good_bucket(rpcargparse):
     '''
     We expect this to pass using xpass with a good filename
     https://docs.pytest.org/en/documentation-restructure/how-to/skipping.html
     '''
-    rpcargparse.parse_args(['-b',bucket_fn_data[0][0]])
+    rpcargparse.parse_args(['-b', bucket_fn_data[0][0]])
 
-@pytest.mark.parametrize('parse',[['-b',bucket_fn_data[1][0]],['-o']])
-def test_bad_arg(parse,rpcargparse):
+
+@pytest.mark.parametrize('parse', [['-b', bucket_fn_data[1][0]], ['-o']])
+def test_bad_arg(parse, rpcargparse):
     '''
     These are tests we expect to fail, bad file, bad syntax etc.
     https://medium.com/python-pandemonium/testing-sys-exit-with-pytest-10c6e5f7726f
@@ -56,11 +63,12 @@ def test_bad_arg(parse,rpcargparse):
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
 
-@pytest.mark.parametrize("parse,key,expected",[
-    (None,'output_fn','%s'%datetime.date.today().strftime('%Y_%m_%d')),
-    (['-o','cool'],'output_fn','cool'),
-    (['-o',''],'output_fn',''),
+
+@pytest.mark.parametrize("parse,key,expected", [
+    (None, 'output_fn', '%s' % datetime.date.today().strftime('%Y_%m_%d')),
+    (['-o', 'cool'], 'output_fn', 'cool'),
+    (['-o', ''], 'output_fn', ''),
 ])
-def test_output_default_val(parse,key,expected,rpcargparse):
+def test_output_default_val(parse, key, expected, rpcargparse):
     args = rpcargparse.parse_args(parse)
     assert vars(args).get(key) == expected
--- tests/test_read_bucket.py
+++ tests/test_read_bucket.py
@@ -5,8 +5,9 @@ import pandas as pd
 import logging
 import numpy as np
 
-good_bucket = os.path.join('Sample_Reports','2_1_18Bucket_Scrubbed.xls')
-mixed_bucket = os.path.join('tests','2_1_18Bucket_Scrubbed_Tester_sheets.xls')
+good_bucket = os.path.join('Sample_Reports', '2_1_18Bucket_Scrubbed.xls')
+mixed_bucket = os.path.join('tests', '2_1_18Bucket_Scrubbed_Tester_sheets.xls')
+
 
 @pytest.fixture
 def good_excel():
@@ -19,9 +20,10 @@ def mixed_excel():
     ''' Returns excel file object for known test bucket'''
     return pd.ExcelFile(mixed_bucket)
 
-def test_non_standard_sheet(caplog,mixed_excel):
-    caplog.set_level(logging.WARNING,logger='report')
-    df = read_bucket_sheet('Tester_CAN31',mixed_excel)
+
+def test_non_standard_sheet(caplog, mixed_excel):
+    caplog.set_level(logging.WARNING, logger='report')
+    df = read_bucket_sheet('Tester_CAN31', mixed_excel)
     for record in caplog.records:
         assert record.levelno >= logging.WARNING
     assert 'Sheetname Tester_CAN31' in caplog.text
@@ -29,45 +31,50 @@ def test_non_standard_sheet(caplog,mixed_excel):
     assert np.issubdtype(df['Date'], np.datetime64)
     assert all(df['Bucket'] == 'Tester_CAN31')
 
-def test_bad_col_name(caplog,mixed_excel):
-    caplog.set_level(logging.WARNING,logger='report')
+
+def test_bad_col_name(caplog, mixed_excel):
+    caplog.set_level(logging.WARNING, logger='report')
     with pytest.raises(KeyError):
-        df = read_bucket_sheet('Tester_CAN31_Bad_Col_name',mixed_excel)
+        read_bucket_sheet('Tester_CAN31_Bad_Col_name', mixed_excel)
 
     for record in caplog.records:
         assert record.levelno >= logging.WARNING
     assert 'specified col' in caplog.text
     assert 'Current Dat' in caplog.text
 
-@pytest.mark.parametrize('sheetname',[
+
+@pytest.mark.parametrize('sheetname', [
     '60 Day',
     'EPD 31+',
     'FPD 2-30',
 ])
-def test_empty_sheets(sheetname,caplog,mixed_excel):
-    caplog.set_level(logging.WARNING,logger='report')
+def test_empty_sheets(sheetname, caplog, mixed_excel):
+    caplog.set_level(logging.WARNING, logger='report')
 
-    df = read_bucket_sheet(sheetname,mixed_excel)
+    read_bucket_sheet(sheetname, mixed_excel)
 
     for record in caplog.records:
         assert record.levelno >= logging.WARNING
     assert 'no account numbers' in caplog.text
 
-def test_bad_date_format(caplog,mixed_excel):
-    caplog.set_level(logging.WARNING,logger='report')
-    df = read_bucket_sheet('Tester_CAN31_dateformat',mixed_excel)
+
+def test_bad_date_format(caplog, mixed_excel):
+    caplog.set_level(logging.WARNING, logger='report')
+    df = read_bucket_sheet('Tester_CAN31_dateformat', mixed_excel)
     for record in caplog.records:
         assert record.levelno >= logging.WARNING
     assert 'Read buckets failed' in caplog.text
     assert np.issubdtype(df['Date'], np.datetime64)
 
-def test_no_date_format(caplog,mixed_excel):
-    caplog.set_level(logging.WARNING,logger='report')
+
+def test_no_date_format(caplog, mixed_excel):
+    caplog.set_level(logging.WARNING, logger='report')
     with pytest.raises(ValueError):
-        df = read_bucket_sheet('Tester_CAN31_no_date',mixed_excel)
+        read_bucket_sheet('Tester_CAN31_no_date', mixed_excel)
     for record in caplog.records:
         assert record.levelno >= logging.WARNING
     assert 'Read buckets failed' in caplog.text
+
 
 def test_working_file():
     df = read_buckets(good_bucket)
